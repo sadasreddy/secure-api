043d3db5dae7d97c7958046496221165709a0ff4
giri <gteella@gmail.com>
2023-03-02 09:50:02 +0000

    adding github actions workflow to run tests-7

--- self-runner/start.sh
+++ self-runner/start.sh
@@ -4,7 +4,7 @@
 #ACCESS_TOKEN=$ACCESS_TOKEN
 #
 #REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-REG_TOKEN=AEX4CWC2BB65LWIJDOA445TEABWT2
+REG_TOKEN=AEX4CWG6ECZNQOXQD6GTU7TEAB6X4
 
 cd /home/docker/actions-runner
 
